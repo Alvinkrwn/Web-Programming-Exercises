2c8bf845408e7a03c8d4c134f0ae6fb495140590
Alvinkrwn <71320028+Alvinkrwn@users.noreply.github.com>
2021-04-26 14:34:53 +0700

    Tugas Praktikum 3

---
+++ TugasPraktikum3/prak030101.php
@@ -0,0 +1,13 @@
+<?php
+
+function gandakanString($teks, $i) {
+        
+        $j = 1;
+        while ($j <= $i) {
+            echo $teks; 
+        $j++;
+        }
+}
+gandakanString("Hello", 3);
+
+?>
---
+++ TugasPraktikum3/prak030103.php
@@ -0,0 +1,16 @@
+<?php
+function buatBintang($n){
+  	echo "<pre>";
+
+        for($i=$n; $i > 0; $i--){
+            for($j=0; $j < $i; $j++){
+                echo "* ";
+            }
+            echo "\n";
+        }
+
+  	echo "</pre>";
+}
+
+buatBintang(4);
+?>
